c22dbbd88191a37bd1d15b3480de0f826166e879
mbalamat <mbalamatsias@gmail.com>
2015-02-18 03:39:50 +0200

    added IP to the file that the forms where submited

--- scripts/modify_form.py
+++ scripts/modify_form.py
@@ -1,10 +1,10 @@
 print "up and running"
 def request(context, flow):
 	with open("forms.txt", "a") as f:
-		#f.write("requested\n")
 		if "application/x-www-form-urlencoded" in flow.request.headers["content-type"]:
+			f.write(str(flow.live.c.server_conn))
 			form = flow.request.get_form_urlencoded()
-			#f.write("incoming\n")
+			f.write("\n")
 			f.write(str(form))
 			f.write("\n")
 			flow.request.set_form_urlencoded(form)
