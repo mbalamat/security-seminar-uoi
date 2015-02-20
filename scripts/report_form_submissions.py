641b7ffca2d4a3a5aad8f4d2047c1ea94a38e017
Kostis Karantias <karantiaskostis@gmail.com>
2015-02-20 03:51:24 +0200

    Fix identation and use context for logging

--- scripts/report_form_submissions.py
+++ scripts/report_form_submissions.py
@@ -1,10 +1,7 @@
-print "up and running"
 def request(context, flow):
-	with open("forms.txt", "a") as f:
-		if "application/x-www-form-urlencoded" in flow.request.headers["content-type"]:
-			f.write(str(flow.live.c.server_conn))
-			form = flow.request.get_form_urlencoded()
-			f.write("\n")
-			f.write(str(form))
-			f.write("\n")
-			flow.request.set_form_urlencoded(form)
+    if "application/x-www-form-urlencoded" in flow.request.headers["content-type"]:
+        context.log("New form submission")
+        context.log(str(flow.live.c.server_conn))
+        form = flow.request.get_form_urlencoded()
+        context.log(str(form))
+        flow.request.set_form_urlencoded(form)
